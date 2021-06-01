87da273273568f748503fc80823c35baf54fad83
bullara2 <bullara2@wwu.edu>
2021-06-01 16:25:11 -0700

    Clients progress

--- Scripts/AllClients.js
+++ Scripts/AllClients.js
@@ -15,10 +15,69 @@ export default function AllClients() {
 
   // Main stage controls.
   const [showActivityIndicator, setActivityIndicator] = useState(true)
+  const [showStats, setShowStats] = useState(false)
+  const [showClients, setShowClients] = useState(false)
+  const [showClientData, setShowClientData] = useState(false)
+
+  // Main variables.
+  const [coach, setCoach] = useState({})
+
+  // Stats variables.
+  // - Client count info.
+  const [clientCount, setClientCount] = useState(0)
+  const [activeClientCount, setActiveClientCount] = useState(0)
+  const [activeClientCountSuffix, setActiveClientCountSuffix] = useState(' / 3 Active Clients')
+
+  // All clients variables.
+  const [clients, setClients] = useState([])
+
+  // Client data variables.
+  const [clientData, setClientData] = useState({})
+
+  // Stats functions.
+
+
+  // All clients functions.
+  const refreshClients = async (id, token) => {
+    var refresh = JSON.parse(JSON.stringify(await getClients(id, token)));
+    // Supply selected variable for group management.
+    var len = refresh.length;
+    var activeLen = 0
+    for (var i = 0; i < len i++) {
+      refresh[i].Selected = false
+      if (refresh[i].Active == true) {
+        activeLen += 1
+      }
+    }
+    setActiveClientCount(activeLen)
+    setClientCount(len)
+    setClients(refresh)
+  }
+
+  // Client data variables.
+
 
   useEffect(() => {
     console.log('Welcome to all clients.')
-  },[])
+    const sCoach = get('Coach')
+    if (sCoach != null) {
+      refreshClients(sCoach.Id, sCoach.Token)
+      // Stats info calculations. //
+      var plan = sCoach.Plan
+      if (plan == 1) {
+        setActiveClientCountSuffix(' / 10 Active Clients')
+      } else if (plan == 2) {
+        setActiveClientCountSuffix(' Active Clients')
+      }
+      // ------------------------ //
+      setCoach(sCoach)
+      setTimeout(() => {
+        setActivityIndicator(false)
+        setShowStats(true)
+        setShowClients(true)
+      }, 500)
+    }
+  }, [])
 
   return (<ScrollView contentContainerStyle={styles.scrollView}>
     <View style={styles.container}>
@@ -33,6 +92,22 @@ export default function AllClients() {
           </View>
 
           {showActivityIndicator && (<ActivityIndicatorView />)}
+
+          {showStats && (<View style={styles.bodyContainer}>
+            <Text></Text>
+          </View>)}
+
+          {showClients && (<View style={styles.bodyContainer}>
+            <Text style={styles.bodySubtitle}>All Clients</Text>
+            <View style={styles.clientOptions}>
+            </View>
+            <View style={styles.clientList}>
+            </View>
+          </View>)}
+
+          {showClientData && (<View style={styles.bodyContainer}>
+            <Text></Text>
+          </View>)}
 
         </View>
       </View>
