db5eb8a24513b075cabc96272d4a87232d869f66
bullara2 <bullara2@wwu.edu>
2021-09-03 15:18:15 -0700

    Started SocialFeed.js, outlined in PB 1.2

--- Scripts/SocialFeed.js
+++ Scripts/SocialFeed.js
@@ -1,18 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useEffect, useState, useContext } from 'react'
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import { socialFeedLight, colorsLight, innerDrawerLight } from '../Scripts/Styles.js'
+import { socialFeedLight, colorsLight, innerDrawerLight, btnColors} from '../Scripts/Styles.js'
 import { socialFeedDark, colorsDark, innerDrawerDark } from '../Scripts/Styles.js'
 import { useLinkTo } from '@react-navigation/native'
 import LoadingScreen from '../Scripts/LoadingScreen.js'
-import { Helmet } from "react-helmet"
 import ActivityIndicatorView from '../Scripts/ActivityIndicatorView.js'
+import { Icon, Button, Chip } from 'react-native-elements'
 
 import userContext from './Context.js'
 
 export default function SocialFeed() {
 
   const user = useContext(userContext)
+
   const linkTo = useLinkTo()
   const [refreshing, setRefreshing] = useState(true)
   const [styles, setStyles] = useState(socialFeedLight)
@@ -20,6 +21,10 @@ export default function SocialFeed() {
 
   // Main stage controls.
   const [showActivityIndicator, setActivityIndicator] = useState(true)
+  const [showSocialFeed, setShowSocialFeed] = useState(false)
+  const [showAddPost, setShowAddPost] = useState(false)
+  const [showSocialFeedPosts, setShowSocialFeedPosts] = useState(false)
+  const [showBio, setShowBio] = useState(false)
 
   // Main variables.
   const [coach, setCoach] = useState(user)
@@ -39,6 +44,32 @@ export default function SocialFeed() {
         <View style={styles.body}>
 
           {showActivityIndicator && (<ActivityIndicatorView />)}
+
+          {showSocialFeed && (<View style={styles.socialFeed}>
+            <View style={styles.socialFeedHeader}>
+              <Text style={styles.socialFeedTitle}>Social Feed</Text>
+              <Button 
+                title='New Post'
+              />
+            </View>
+            <View style={styles.socialFeedPages}>
+              <View style={styles.socialFeedTabination}>
+              </View>
+              {showSocialFeedPosts && (<View style={styles.socialFeedContainer}>
+                <View style={styles.socialFeedMain}>
+                </View>
+                <View style={styles.socialFeedColumn}>
+                </View>  
+              </View>)}
+              {showBio && (<View style={styles.socialFeedBio}>
+              
+              </View>)}
+            </View>
+          </View>)}
+
+          {showAddPost && (<View style={styles.newPostContainer}>
+
+          </View>)}
 
         </View>
       </View>
